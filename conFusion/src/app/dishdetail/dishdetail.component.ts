0d80d8e902bf55199b111804ebf5b57ace2ce9df
reymesson1 <reymesson1>
2021-02-21 20:00:51 -0400

    done

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dishdetail',
@@ -7,15 +10,24 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 
+
 export class DishdetailComponent implements OnInit {
 
+  dish: Dish;
 
-
-  @Input() dish: Dish;
-
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    var id : any = +this.route.snapshot.params['id'];
+    console.log(id);
+    this.dish = this.dishservice.getDish(id);
+    console.log(this.dish);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
