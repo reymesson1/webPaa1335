ec864630c78c330c902ac05d61a752641e2774d4
reymesson1 <reymesson1>
2021-02-24 18:47:49 -0400

    done

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { delay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,26 +12,32 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES), 2000);
-    });
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(DISHES), 2000);
+    // });
     // return Promise.resolve(DISHES);
+    return of(DISHES).pipe(delay(2000)).toPromise();
+
   }
 
   getDish(id: number): Promise<Dish> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
-    });
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    // });
     // return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000)).toPromise();
+
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
-    });
+    // return  new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+    // });
+    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000)).toPromise();
+
   }
   
 }
