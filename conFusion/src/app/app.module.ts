5364dba600fb961c587c7b5ea3ae83583fd64041
reymesson1 <reymesson1>
2021-02-25 13:51:56 -0400

    done

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material/slider';
 
 
 
@@ -66,7 +67,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatSelectModule,
     MatSlideToggleModule,
     ReactiveFormsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent],
--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,9 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { Comment } from '../shared/comment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -18,22 +21,73 @@ export class DishdetailComponent implements OnInit {
   dishIds: string[];
   prev: string;
   next: string;
+  comment: Comment;
+  commentForm: FormGroup;
+
+  formErrors : any = {
+    'author' : '',
+    'rating' : '',
+    'comment' : ''
+  };
+
+  validationMessages : any = {
+    'author' : {
+      'required' : 'Name is required',
+      'minlength' : 'Name must be at least 2 characters long',
+      'maxlength' : 'Name cannot be more that 25 characters long'
+    }
+  };
+
 
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,     
+    private fb: FormBuilder) { 
 
-  // ngOnInit() {
-  //   var id = +this.route.snapshot.params['id'];
-  //   console.log(id);
-  //   // this.dish = this.dishservice.getDish(id);
-  //   // this.dishservice.getDish(id).then(dish => this.dish = dish);
-  //   this.dishservice.getDish(id.toString()).subscribe(dishes => this.dish = dishes);
+      this.createForm();
 
-  //   console.log(this.dish);
-  // }
-  // Exercise (Instructions): Angular and RxJS Part 2
+    }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [ Validators.required, Validators.minLength(2) ] ],
+      rating: 5,
+      comment: ['', [ Validators.required ] ],
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set form validation messages
+  }
+
+  onValueChanged(commentFormData?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+
+    if (this.commentForm.valid) {
+      this.comment = this.commentForm.value;
+    } else {
+      this.comment = undefined;
+    }
+  }
+
+  
+
+
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
@@ -48,5 +102,16 @@ export class DishdetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  onSubmit() {
+    this.comment['date'] = new Date().toISOString();
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+        author: '',
+        rating: 5,
+        comment: ''
+    });
+  }
+
 
 }
